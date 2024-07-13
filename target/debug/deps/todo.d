2c4c0c92533e795ec498197166f2a7876f8d63a5
gravity <gravity.chiffon+tech@gmail.com>
2024-07-13 15:33:48 +0900

    init

---
+++ target/debug/deps/todo.d
@@ -0,0 +1,5 @@
+C:\Users\mrhd\Desktop\jikken\rust\rust_todo\target\debug\deps\todo.exe: src/main.rs
+
+C:\Users\mrhd\Desktop\jikken\rust\rust_todo\target\debug\deps\todo.d: src/main.rs
+
+src/main.rs:
---
+++ target/debug/todo.d
@@ -0,0 +1,1 @@
+C:\Users\mrhd\Desktop\jikken\rust\rust_todo\target\debug\todo.exe: C:\Users\mrhd\Desktop\jikken\rust\rust_todo\src\main.rs
