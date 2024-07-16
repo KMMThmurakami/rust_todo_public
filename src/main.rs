61baba81f3e3cb13cf49534554f0996a92a14a29
hideaki.murakami <hideaki.murakami@nijibox.co.jp>
2024-07-16 10:46:15 +0900

    tokio Chapter 04

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,29 @@
-use warp::Filter;
+use tokio::net::{TcpListener, TcpStream};
+use mini_redis::{Connection, Frame};
 
 #[tokio::main]
 async fn main() {
-    let hello = warp::path::end().map(|| format!("Hello, warp!"));
+    // リスナーをこのアドレスにバインドする
+    let listener = TcpListener::bind("127.0.0.1:6379").await.unwrap();
 
-    warp::serve(hello).run(([127, 0, 0, 1], 3030)).await;
+    loop {
+        // タプルの2つ目の要素は、新しいコネクションのIPとポートの情報を含んでいる
+        let (socket, _) = listener.accept().await.unwrap();
+        process(socket).await;
+    }
+}
+
+async fn process(socket: TcpStream) {
+    // `Connection` を使うことで、バイト列ではなく、Redis の
+    // 「フレーム」を読み書きできるようになる
+    // この `Connection` 型は mini-redis で定義されている
+    let mut connection = Connection::new(socket);
+
+    if let Some(frame) = connection.read_frame().await.unwrap() {
+        println!("GOT: {:?}", frame);
+
+        // エラーを返す
+        let response = Frame::Error("unimplemented".to_string());
+        connection.write_frame(&response).await.unwrap();
+    }
 }
