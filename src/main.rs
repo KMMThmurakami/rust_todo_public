9b42d8ccaef365d673d5d9021caa34177c9823c6
hideaki.murakami <hideaki.murakami@nijibox.co.jp>
2024-07-22 18:41:50 +0900

    axum hello world

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,20 @@
-fn main() {
-    println!("main");
-    // rust言語入門を進めていく
+use axum::{routing::get, Router};
+
+#[tokio::main]
+async fn main() {
+    let app = Router::new().route("/", get(root));
+
+    // axum 0.4.8
+    // let addr = SocketAddr::from(([127, 0, 0, 1], 3000));
+    // axum::Server::bind(&addr).serve(app.into_make_service()).await.unwrap();
+
+    // axum 0.7.5
+    let listener = tokio::net::TcpListener::bind("127.0.0.1:3000")
+        .await
+        .unwrap();
+    axum::serve(listener, app).await.unwrap();
+}
+
+async fn root() -> &'static str {
+    "Hello, world!"
 }
