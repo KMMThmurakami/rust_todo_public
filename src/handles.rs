e5a6020864a5b144110de85ad70a59d4f8d70dd9
hideaki.murakami <hideaki.murakami@nijibox.co.jp>
2024-07-26 10:53:37 +0900

    空のハンドラを作成

--- src/handles.rs
+++ src/handles.rs
@@ -1,5 +1,5 @@
-use crate::repositories::{CreateTodo, TodoRepository};
-use axum::{http::StatusCode, response::IntoResponse, Extension, Json};
+use crate::repositories::{CreateTodo, TodoRepository, UpdateTodo};
+use axum::{extract::Path, http::StatusCode, response::IntoResponse, Extension, Json};
 use std::sync::Arc;
 
 pub async fn create_todo<T: TodoRepository>(
@@ -9,4 +9,34 @@ pub async fn create_todo<T: TodoRepository>(
     let todo = repository.create(payload);
 
     (StatusCode::CREATED, Json(todo))
+}
+
+pub async fn find_todo<T: TodoRepository>(
+    Path(id): Path<i32>,
+    Extension(repository): Extension<Arc<T>>,
+) -> Result<impl IntoResponse, StatusCode> {
+    todo!();
+    Ok(StatusCode::OK)
+}
+
+pub async fn all_todo<T: TodoRepository>(
+    Extension(repository): Extension<Arc<T>>,
+) -> impl IntoResponse {
+    todo!();
+}
+
+pub async fn update_todo<T: TodoRepository>(
+    Path(id): Path<i32>,
+    Json(payload): Json<UpdateTodo>,
+    Extension(repository): Extension<Arc<T>>,
+) -> Result<impl IntoResponse, StatusCode> {
+    todo!();
+    Ok(StatusCode::OK)
+}
+
+pub async fn delete_todo<T: TodoRepository>(
+    Path(id): Path<i32>,
+    Extension(repository): Extension<Arc<T>>,
+) -> StatusCode {
+    todo!();
 }
