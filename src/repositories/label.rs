6c85440af33dc4f5ba162211d5eb308d3d36fc9b
hideaki.murakami <hideaki.murakami@nijibox.co.jp>
2024-07-31 15:29:42 +0900

    LabelRepositoryForDb 実装

--- src/repositories/label.rs
+++ src/repositories/label.rs
@@ -1,6 +1,8 @@
+use super::RepositoryError;
+use anyhow::Ok;
 use axum::async_trait;
 use serde::{Deserialize, Serialize};
-use sqlx::PgPool;
+use sqlx::{FromRow, PgPool};
 
 #[async_trait]
 pub trait LabelRepository: Clone + std::marker::Send + std::marker::Sync + 'static {
@@ -9,13 +11,13 @@ pub trait LabelRepository: Clone + std::marker::Send + std::marker::Sync + 'stat
     async fn delete(&self, id: i32) -> anyhow::Result<()>;
 }
 
-#[derive(Debug, Serialize, Deserialize, Clone, PartialEq, Eq)]
+#[derive(Debug, Serialize, Deserialize, Clone, PartialEq, Eq, FromRow)]
 pub struct Label {
     pub id: i32,
     pub name: String,
 }
 
-#[derive(Debug, Serialize, Deserialize, Clone, PartialEq, Eq)]
+#[derive(Debug, Serialize, Deserialize, Clone, PartialEq, Eq, FromRow)]
 pub struct UpdateLabel {
     id: i32,
     name: String,
@@ -35,15 +37,58 @@ impl LabelRepositoryForDb {
 #[async_trait]
 impl LabelRepository for LabelRepositoryForDb {
     async fn create(&self, name: String) -> anyhow::Result<Label> {
-        todo!()
+        let optional_label = sqlx::query_as::<_, Label>(
+            r#"
+                select * from labels where name=$1;
+            "#,
+        )
+        .bind(name.clone())
+        .fetch_optional(&self.pool)
+        .await?;
+
+        if let Some(label) = optional_label {
+            return Err(RepositoryError::Duplicate(label.id).into());
+        }
+
+        let label = sqlx::query_as::<_, Label>(
+            r#"
+                insert into labels (name) values ($1) returning *;
+            "#,
+        )
+        .bind(name.clone())
+        .fetch_one(&self.pool)
+        .await?;
+
+        Ok(label)
     }
 
     async fn all(&self) -> anyhow::Result<Vec<Label>> {
-        todo!()
+        let labels = sqlx::query_as::<_, Label>(
+            r#"
+                select * from labels order by labels.id asc;
+            "#,
+        )
+        .fetch_all(&self.pool)
+        .await?;
+
+        Ok(labels)
     }
 
     async fn delete(&self, id: i32) -> anyhow::Result<()> {
-        todo!()
+        sqlx::query(
+            r#"
+                delete from labels where id=$1;
+            "#,
+        )
+        .bind(id)
+        .execute(&self.pool)
+        .await
+        .map_err(|e| match e {
+            sqlx::Error::RowNotFound => RepositoryError::NotFound(id),
+            _ => RepositoryError::Unexpected(e.to_string()),
+        })?;
+
+        Ok(())
     }
 }
 
